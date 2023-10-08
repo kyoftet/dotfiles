962852e533fee5a7f336b697d6892900979c85a4
kyoF <fujiki.kyosuke@gmail.com>
2023-10-08 16:45:33 +0900

    fix: vscode.sh

--- script/vscode.sh
+++ script/vscode.sh
@@ -2,15 +2,15 @@
 MYVSCODE=~/dotfiles/vscode
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
-if [[ -e "$VSCODE_SETTING_DIR/settings.json" ]]; then
+if [[ -L "$VSCODE_SETTING_DIR/settings.json" ]]; then
   rm "$VSCODE_SETTING_DIR/settings.json"
 fi
-ln -s "$MYVSCODE/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+ln -fsvn "$MYVSCODE/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-if [[ -e "$VSCODE_SETTING_DIR/keybindings.json" ]]; then
+if [[ -L "$VSCODE_SETTING_DIR/keybindings.json" ]]; then
   rm "$VSCODE_SETTING_DIR/keybindings.json"
 fi
-ln -s "$MYVSCODE/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
+ln -fsvn "$MYVSCODE/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
 cat $MYVSCODE/extensions | while read line
 do
