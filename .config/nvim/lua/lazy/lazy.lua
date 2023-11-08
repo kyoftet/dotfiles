4747b9f3377cb715f07795bbfe85a7d7d9cf28e0
kyosukeFujiki <fujiki.kyosuke.tl@mynavi.jp>
2023-11-08 17:25:06 +0900

    add: gitsign plugin

--- .config/nvim/lua/lazy/lazy.lua
+++ .config/nvim/lua/lazy/lazy.lua
@@ -97,8 +97,58 @@ require('lazy').setup({
     'nvim-telescope/telescope.nvim', tag = '0.1.3',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      vim.keymap.set('n', '<leader>p', ':Telescope find_files<cr>')
+      vim.keymap.set('n', '<leader>p', ':Telescope find_files hidden=true<cr>')
       vim.keymap.set('n', '<leader>f', ':Telescope live_grep<cr>')
+      vim.keymap.set('n', '<leader>gs', ':Telescope git_status<cr>')
+      vim.keymap.set('n', '<leader>gl', ':Telescope git_commits<cr>')
+    end
+  },
+  -- git
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function ()
+      require('gitsigns').setup {
+        signs = {
+          add          = { hl = 'GitSignsAdd'   , text = ' ▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'    },
+          change       = { hl = 'GitSignsChange', text = ' ▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+          delete       = { hl = 'GitSignsDelete', text = ' ', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+          topdelete    = { hl = 'GitSignsDelete', text = ' ', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn' },
+          changedelete = { hl = 'GitSignsChange', text = '▎ ', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn' },
+        },
+        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        watch_gitdir = {
+          interval = 1000,
+          follow_files = true
+        },
+        attach_to_untracked = true,
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+        sign_priority = 6,
+        update_debounce = 100,
+        status_formatter = nil, -- Use default
+        max_file_length = 40000, -- Disable if file is longer than this (in lines)
+        preview_config = {
+          -- Options passed to nvim_open_win
+          border = 'single',
+          style = 'minimal',
+          relative = 'cursor',
+          row = 0,
+          col = 1
+        },
+        yadm = {
+          enable = false
+        },
+      }
     end
   }
 })
+
