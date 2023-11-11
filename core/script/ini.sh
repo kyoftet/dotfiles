80a3c1a359a67cc0434fe3c63d8e1b30eb5151ac
kyoF <83649546+kyoF@users.noreply.github.com>
2023-11-12 03:31:46 +0900

    Update ini.sh

--- core/script/ini.sh
+++ core/script/ini.sh
@@ -11,9 +11,13 @@ while true; do
   read -p "Your Github Email Address: " input_mail_address
   read -p "Correct? [y/n]: " validation
   case $validation in
-    [yY]|[yY][eE][sS]) echo "continue"; break ;;
-    [nN]|[nN][oO]) echo "please input again";;
-    *) echo "please input again";;
+    [yY]|[yY][eE][sS])
+      echo "continue";
+      break ;;
+    [nN]|[nN][oO])
+      echo "please input again";;
+    *)
+      echo "please input again";;
   esac
 done
 export GITHUB_USER_MAIL=$input_name
