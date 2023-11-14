e620877e96f4501406630c7496dd3ad9c0ef7a96
kyosukeFujiki <fujiki.kyosuke.tl@mynavi.jp>
2023-11-14 12:12:14 +0900

    add: back

--- script/linux/ini.sh
+++ script/linux/ini.sh
@@ -2,9 +2,10 @@
 cd
 
 # homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
+
 install_app=(
   gcc
   zsh
@@ -24,30 +25,42 @@ done
 echo ======== finish brew install ========
 
 # zsh
-sudo chsh -s "$(which zsh)"
-# sleep 10
-echo ======== finish setup zsh ========
+sudo chsh -s "$(echo $SHELL)"
+sleep 10
 
 # devbox
 curl -fsSL https://get.jetpack.io/devbox | bash
 echo ======== finish install devbox ========
 
 # git
+while true; do
+  read -p "your github user name: " input_name
+  read -p "your github email address: " input_mail_address
+
+  read -p "Correct? [y/n]: " validation
+
+  case $validation in
+    [yY]|[yY][eE][sS])
+      echo "Continue...";
+      break ;;
+    [nN]|[nN][oO])
+      echo "please input again!";;
+    *)
+      echo "faild"
+      echo "please input again!"
+  esac
+done
+export GITHUB_USER_NAME=$input_name
+export GITHUB_USER_MAIL=$input_mail_address
 echo "[user]
-  name = $1
-  email = $2
-" >> ~/.gitconfig
-echo "[user]
-  name = $1
-  email = $2
+  name = $GITHUB_USER_NAME
+  email = $GITHUB_USER_MAIL
 " >> ~/.gitconfig.local
-ssh-keygen -t ed25519 -C "kyof-dotfiles"
-git clone git@github.com:kyoF/dotfiles.git
 echo ======== finish setup git ========
 
 # symbolic link 
 cd ~/dotfiles
-./script/linux/symbolic.sh
+script/utils/symbolic.sh
 echo ======== finish setup git ========
 
 echo ============================
