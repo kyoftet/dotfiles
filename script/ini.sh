9be587de1ddb615941d4605eb34f9c0b89a251a2
kyoF <83649546+kyoF@users.noreply.github.com>
2023-11-14 18:00:58 +0900

    Update ini.sh

--- script/ini.sh
+++ script/ini.sh
@@ -8,7 +8,6 @@ if [ "$(uname)" != "Darwin" ]; then
   elif command -v apt-get &> /dev/null; then
     sudo apt-get update
     sudo apt-get install zsh
-    zsh
   else
     echo "Error: Neither yum nor apt foun."
     echo "Unable to install zsh."
