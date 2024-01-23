815d0ff9524a5bfacdaaf38cda2d1e52aa304beb
kyosukeFujiki <fujiki.kyosuke.tl@mynavi.jp>
2024-01-23 11:58:16 +0900

    add: markdown preview

--- .config/nvim/lua/lazy/lazy.lua
+++ .config/nvim/lua/lazy/lazy.lua
@@ -224,5 +224,12 @@ require('lazy').setup({
       }
     end
   },
+  -- markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 })
 
