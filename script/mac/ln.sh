8a7d68a55db60ca5954d69dc69fbb1df3e744bc5
kyosukeFujiki <fujiki.kyosuke.tl@mynavi.jp>
2024-02-06 13:55:23 +0900

    del: karabiner

--- script/mac/ln.sh
+++ script/mac/ln.sh
@@ -5,6 +5,10 @@ cd $HOME/dotfiles
 for f in .??*; do
   [[ $f == ".git" ]] && continue
   [[ $f == ".gitignore" ]] && continue
+  if [[ $f == ".config" ]]; then
+    ln -snfv ${PWD}/"$f"/nvim ~/.config/
+    continue
+  fi
   ln -snfv ${PWD}/"$f" ~/
 done
 
