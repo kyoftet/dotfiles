c55e9ae1f0f497ad7a88200964b8c832a9f416a4
kyoF <fujiki.kyosuke@gmail.com>
2024-03-19 00:11:02 +0900

    rename: linux -> ubuntu

---
+++ script/ubuntu/ln.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd $HOME/dotfiles
+
+for f in .??*; do
+  [[ $f == ".git" ]] && continue
+  [[ $f == ".gitignore" ]] && continue
+  [[ $f == ".hammerspoon" ]] && continue
+  if [[ $f == ".config" ]]; then
+    ln -snfv ${PWD}/"$f"/nvim ~/.config/
+    ln -snfv ${PWD}/"$f"/starship ~/.config/
+    ln -snfv ${PWD}/"$f"/alacritty ~/.config/
+    ln -snfv ${PWD}/"$f"/git ~/.config/
+    ln -snfv ${PWD}/"$f"/tmux ~/.config/
+    ln -snfv ${PWD}/"$f"/wezterm ~/.config/
+    ln -snfv ${PWD}/"$f"/zellij ~/.config/
+    ln -snfv ${PWD}/"$f"/zsh ~/.config/
+    continue
+  fi
+  ln -snfv ${PWD}/"$f" ~/
+done
+
+source ~/.zshrc
