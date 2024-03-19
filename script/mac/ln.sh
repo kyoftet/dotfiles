89bc9dcb5b6b6be0c07045ce032ce9d7ae5a30f8
kyoF <fujiki.kyosuke@gmail.com>
2024-03-19 14:13:20 +0900

    fix: ln.sh for mac

--- script/mac/ln.sh
+++ script/mac/ln.sh
@@ -7,10 +7,15 @@ for f in .??*; do
   [[ $f == ".gitignore" ]] && continue
   if [[ $f == ".config" ]]; then
     ln -snfv ${PWD}/"$f"/nvim ~/.config/
+    ln -snfv ${PWD}/"$f"/starship ~/.config/
+    ln -snfv ${PWD}/"$f"/alacritty ~/.config/
+    ln -snfv ${PWD}/"$f"/tmux ~/.config/
+    ln -snfv ${PWD}/"$f"/wezterm ~/.config/
+    ln -snfv ${PWD}/"$f"/zellij ~/.config/
+    ln -snfv ${PWD}/"$f"/karabiner ~/.config/
     continue
   fi
   ln -snfv ${PWD}/"$f" ~/
 done
 
 source ~/.zshrc
-
