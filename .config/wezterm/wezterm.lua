86f2dcdc0d615f093707406c35503aabb28f85e0
kyoF <fujiki.kyosuke@gmail.com>
2024-03-19 09:27:22 +0900

    fix: window_frame on wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -119,9 +119,29 @@ config.colors = {
         new_tab = { bg_color = "#121212", fg_color = "#FCE8C3", intensity = "Bold" },
     }
 }
+-- config.window_frame = {
+--     inactive_titlebar_bg = 'rgba(0,255,255,0)',
+--     active_titlebar_bg = 'rgba(0,255,255,0)',
+-- }
 config.window_frame = {
-    inactive_titlebar_bg = 'rgba(0,0,0,0)',
-    active_titlebar_bg = 'rgba(0,0,0,0)',
+  inactive_titlebar_bg = '#353535',
+  active_titlebar_bg = '#2b2042',
+  inactive_titlebar_fg = '#cccccc',
+  active_titlebar_fg = '#ffffff',
+  inactive_titlebar_border_bottom = '#2b2042',
+  active_titlebar_border_bottom = '#2b2042',
+  button_fg = '#cccccc',
+  button_bg = '#2b2042',
+  button_hover_fg = '#ffffff',
+  button_hover_bg = '#3b3052',
+  border_left_width = '0.5cell',
+  border_right_width = '0.5cell',
+  border_bottom_height = '0.25cell',
+  border_top_height = '0.25cell',
+  border_left_color = 'purple',
+  border_right_color = 'purple',
+  border_bottom_color = 'purple',
+  border_top_color = 'purple',
 }
 
 config.keys = {
