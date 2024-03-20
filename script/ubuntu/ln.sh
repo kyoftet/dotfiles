68c3f8a610d6b94cfb634264e77069075d8a7811
kyoF <fujiki.kyosuke@gmail.com>
2024-03-21 01:33:52 +0900

    fix: ubuntu ln.sh

--- script/ubuntu/ln.sh
+++ script/ubuntu/ln.sh
@@ -10,11 +10,9 @@ for f in .??*; do
     ln -snfv ${PWD}/"$f"/nvim ~/.config/
     ln -snfv ${PWD}/"$f"/starship ~/.config/
     ln -snfv ${PWD}/"$f"/alacritty ~/.config/
-    ln -snfv ${PWD}/"$f"/git ~/.config/
     ln -snfv ${PWD}/"$f"/tmux ~/.config/
     ln -snfv ${PWD}/"$f"/wezterm ~/.config/
     ln -snfv ${PWD}/"$f"/zellij ~/.config/
-    ln -snfv ${PWD}/"$f"/zsh ~/.config/
     continue
   fi
   ln -snfv ${PWD}/"$f" ~/
