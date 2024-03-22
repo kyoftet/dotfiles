21f34f3d8061b4e70204495d5fc26de252745765
kyoF <fujiki.kyosuke@gmail.com>
2024-03-22 17:28:22 +0900

    add: autocmds

---
+++ .config/nvim/lua/core/autocmds.lua
@@ -0,0 +1,26 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "py",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end
+})
