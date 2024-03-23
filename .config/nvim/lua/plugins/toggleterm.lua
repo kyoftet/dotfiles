58a3b5bf1d1a88b2a09c617ae501f67c515843d6
kyoF <fujiki.kyosuke@gmail.com>
2024-03-23 17:21:53 +0900

    fix: toggleterm

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,8 +2,16 @@ return {
     'akinsho/toggleterm.nvim',
     tag = "*",
     config = function()
+        local Terminal = require('toggleterm.terminal').Terminal
+        local lazygit  = Terminal:new({ cmd = "lazygit", dir = 'git_dir', direction = 'float', hidden = true })
+        function _lazygit_toggle()
+            lazygit:toggle()
+        end
+
         require('toggleterm').setup {
-            vim.keymap.set('n', '<leader>tt', ':ToggleTerm size=50 direction=float<cr>'),
+            vim.keymap.set('n', 'tp', ':ToggleTerm direction=float<cr>'),
+            vim.keymap.set('t', 'te', '<C-\\><C-n>:ToggleTerm<cr>'),
+            vim.api.nvim_set_keymap("n", "tlg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true }),
         }
     end
 }
