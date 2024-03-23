70399dc8d8608325f5b4e872c51d68ac78432ff5
kyoF <fujiki.kyosuke@gmail.com>
2024-03-23 17:59:44 +0900

    fix: telescope keymap

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,12 +2,15 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.3',
     dependencies = { 'nvim-lua/plenary.nvim' },
+
     config = function()
         vim.keymap.set('n', 'tf', ':Telescope find_files hidden=true<cr>')
         vim.keymap.set('n', 'tg', ':Telescope live_grep<cr>')
+        vim.keymap.set('n', 'th', ':Telescope help_tags<cr>')
         vim.keymap.set('n', 'tgs', ':Telescope git_status<cr>')
         vim.keymap.set('n', 'tgc', ':Telescope git_commits<cr>')
         vim.keymap.set('n', 'tk', ':Telescope keymaps<cr>')
         vim.keymap.set('n', 'tb', ':Telescope buffers<cr>')
+        vim.keymap.set('n', 'ta', ':Telescope treesitter<cr>')
     end
 }
