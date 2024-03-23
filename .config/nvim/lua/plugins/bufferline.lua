aa6a5d4ad9309e729da917897d5daaa7bb7c4c6e
kyoF <fujiki.kyosuke@gmail.com>
2024-03-23 16:27:03 +0900

    fix: bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -5,8 +5,11 @@ return {
     config = function()
         require('bufferline').setup({
             highlights = {
+                close_button = { fg = '#00cccc' },
+                close_button_visible = { fg = '#00cccc' },
+                close_button_selected = { fg = '#00cccc' },
                 buffer_selected = { fg = '#00cccc', bold = true },
-                buffer_visible = { fg = '#4169e1', bold = true },
+                buffer_visible = { fg = '#4169e1' },
                 diagnostic_selected = { bold = true },
                 info_selected = { bold = true },
                 info_diagnostic_selected = { bold = true },
@@ -14,18 +17,26 @@ return {
                 warning_diagnostic_selected = { bold = true },
                 error_selected = { bold = true },
                 error_diagnostic_selected = { bold = true },
+                separator_selected = { fg = '#456789' },
+                separator_visible = { fg = '#456789' },
+                separator = { fg = '#456789' },
             },
             options = {
+                indicator = { style = 'underline' },
                 buffer_close_icon = 'x',
-                diagnostics = 'nvim_lsp',
-                diagnostics_indicator = function(count, level, diagnostics_dict, context)
+                diagnostics = 'coc',
+                diagnostics_indicator = function(count, level)
                     local icon = level:match("error") and " " or " "
                     return " " .. icon .. count
-                end
+                end,
             }
         })
+        vim.keymap.set('n', '<leader>bp', '<CMD>BufferLinePick<CR>')
         vim.keymap.set('n', '<leader>bc', '<CMD>BufferLinePickClose<CR>')
-        vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>')
-        vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>')
+        vim.keymap.set('n', '<leader>bl', '<CMD>BufferLineCloseLeft<CR>')
+        vim.keymap.set('n', '<leader>br', '<CMD>BufferLineCloseRight<CR>')
+        vim.keymap.set('n', '<leader>bo', '<CMD>BufferLineCloseOthers<CR>')
+        vim.keymap.set('n', '<leader>bm', '<CMD>BufferLineCycleNext<CR>')
+        vim.keymap.set('n', '<leader>bn', '<CMD>BufferLineCyclePrev<CR>')
     end
 }
