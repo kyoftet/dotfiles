3ed912cd406471590d0014c532636d6b757cf8d3
kyoF <fujiki.kyosuke@gmail.com>
2024-03-23 14:27:20 +0900

    fix: filename

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,27 +1,27 @@
 local application = require("hs.application")
 local spaces = require("hs.spaces")
 
-local double_press = require("ctrlDoublePress")
+local double_press = require("ctrl_double_press")
 
 local open_alacritty = function()
-	local appName = "Alacritty"
-	local app = application.get(appName)
+    local appName = "Alacritty"
+    local app = application.get(appName)
 
-	if app == nil then
-		application.launchOrFocus(appName)
-	elseif app:isFrontmost() then
-		app:hide()
-	else
-		local active_space = spaces.focusedSpace()
-		local alacritty_win = app:focusedWindow()
-		spaces.moveWindowToSpace(alacritty_win, active_space)
-		app:setFrontmost()
-	end
+    if app == nil then
+        application.launchOrFocus(appName)
+    elseif app:isFrontmost() then
+        app:hide()
+    else
+        local active_space = spaces.focusedSpace()
+        local alacritty_win = app:focusedWindow()
+        spaces.moveWindowToSpace(alacritty_win, active_space)
+        app:setFrontmost()
+    end
 end
 
 double_press.timeFrame = 0.5
 double_press.action = open_alacritty
 
 hs.hotkey.bind({ "alt" }, "u", function()
-	hs.execute("$HOME/.config/alacritty/bin/toggle_opacity.sh", true)
+    hs.execute("$HOME/.config/alacritty/bin/toggle_opacity.sh", true)
 end)
