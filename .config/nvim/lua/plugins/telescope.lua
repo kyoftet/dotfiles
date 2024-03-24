39430e0fec99bc293d4acb46a9f05f8d5373f946
kyoF <fujiki.kyosuke@gmail.com>
2024-03-25 01:10:05 +0900

    fix: telescope keymap

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,15 +2,28 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.3',
     dependencies = { 'nvim-lua/plenary.nvim' },
-
     config = function()
-        vim.keymap.set('n', 'tf', ':Telescope find_files hidden=true<cr>')
-        vim.keymap.set('n', 'tg', ':Telescope live_grep<cr>')
-        vim.keymap.set('n', 'th', ':Telescope help_tags<cr>')
-        vim.keymap.set('n', 'tgs', ':Telescope git_status<cr>')
-        vim.keymap.set('n', 'tgc', ':Telescope git_commits<cr>')
-        vim.keymap.set('n', 'tk', ':Telescope keymaps<cr>')
-        vim.keymap.set('n', 'tb', ':Telescope buffers<cr>')
-        vim.keymap.set('n', 'ta', ':Telescope treesitter<cr>')
+        local actions = require("telescope.actions")
+        require("telescope").setup({
+            defaults = {
+                initial_mode = 'normal',
+                mappings = {
+                    i = {
+                        ["<C-h>"] = actions.select_horizontal,
+                    },
+                    n = {
+                        ["<C-c>"] = actions.close,
+                        ["<C-h>"] = actions.select_horizontal,
+                    }
+                },
+            },
+        })
+        vim.keymap.set('n', '<C-f>f', ':Telescope find_files hidden=true<cr>')
+        vim.keymap.set('n', '<C-f>l', ':Telescope live_grep<cr>')
+        vim.keymap.set('n', '<C-f>h', ':Telescope help_tags<cr>')
+        vim.keymap.set('n', '<C-f>gs', ':Telescope git_status<cr>')
+        vim.keymap.set('n', '<C-f>gc', ':Telescope git_commits<cr>')
+        vim.keymap.set('n', '<C-f>k', ':Telescope keymaps<cr>')
+        vim.keymap.set('n', '<C-f>b', ':Telescope buffers<cr>')
     end
 }
