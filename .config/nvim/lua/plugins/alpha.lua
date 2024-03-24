78d6eb6786ead69749721af74386f5373eb655af
kyoF <fujiki.kyosuke@gmail.com>
2024-03-25 01:08:56 +0900

    fix: alpha

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -19,6 +19,7 @@ return {
             dashboard.button("f", "  Find file", ":Telescope find_files hidden=true<cr>"),
             dashboard.button("h", "󰋖  Find help", ":Telescope help_tags<cr>"),
             dashboard.button("e", "  View dir", ":Neotree float<cr>"),
+            dashboard.button("k", "  View keymaps", ":Telescope keymaps<cr>"),
             dashboard.button("l", "  Check lsp", ":Mason<cr>"),
             dashboard.button("t", "  Toggle terminal", ":ToggleTerm direction=float<cr>"),
             dashboard.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
