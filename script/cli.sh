95f85bedc4a7c0881d60fe867363d839368e5845
kyoF <fujiki.kyosuke@gmail.com>
2024-04-03 22:34:26 +0900

    fix: rust cli

--- script/cli.sh
+++ script/cli.sh
@@ -1,3 +1,5 @@
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
 cargo install bat
 cargo install fd-find
 cargo install genact
@@ -5,3 +7,5 @@ cargo install glitchcat
 cargo install lsd
 cargo install ripgrep
 cargo install xh
+cargo install mise
+cargo install sheldon
