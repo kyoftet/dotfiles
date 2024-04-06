7bd30850a1d9ff2ab7672d5d88efe1be45e2e006
kyoF <fujiki.kyosuke@gmail.com>
2024-04-06 23:29:47 +0900

    add: ini.sh for linux

---
+++ script/linux/ini.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# xcode
+xcode-select --install
+
+# dotfiles
+git clone https://github.com/kyoF/dotfiles.git
+
+# symbolic link 
+cd $HOME/dotfiles
+./script/ln.sh
+
+# git
+git remote set-url origin git@github.com:kyoF/dotfiles.git
+while true; do
+  read -p "your github user name: " name
+  read -p "your github email address: " mail
+
+  read -p "Correct? [y/n]: " validation
+
+  case $validation in
+    [yY]|[yY][eE][sS])
+      echo "Continue...";
+      break ;;
+    [nN]|[nN][oO])
+      echo "please input again!";;
+    *)
+      echo "faild"
+      echo "please input again!"
+  esac
+done
+echo "[user]
+  name = $name
+  email = $mail
+" > ~/.gitconfig.local
+
+# rust
+./script/rust.sh
