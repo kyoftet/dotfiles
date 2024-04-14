4b52e092625e7011597ae5c25888df79a8b9ce49
kyoF <fujiki.kyosuke@gmail.com>
2024-04-14 19:06:05 +0900

    add: ln_tmp.sh

--- script/ln_tmp.sh
+++ script/ln_tmp.sh
@@ -1,6 +1,25 @@
-#!/bin/bash
+#!/bin/zsh
 
 cd $HOME/dotfiles
+
+os=$(uname)
+declare -A config
+
+if [[ "$os" == "Darwin" ]]; then
+  config_file="./script/mac/config.txt"
+else
+  config_file="./script/linux/config.txt"
+fi
+
+while read -r line; do
+  [[ $line =~ ^#.* ]] && continue
+    
+  if [[ $line =~ ^([^=]+)=(.*)$ ]]; then
+    key=${match[1]}
+    value=${match[2]}
+    config[$key]=$value
+  fi
+done < "$config_file"
 
 for f in .??*; do
   [[ $f == ".git" ]] && continue
