51657ee95a190bd05a7839c8c1f9e598f1e18632
kyoF <fujiki.kyosuke@gmail.com>
2024-07-21 18:19:54 +0900

    fix: ini.sh

--- script/mac/ini.sh
+++ script/mac/ini.sh
@@ -1,43 +1,27 @@
 #!/bin/bash
 
+echo "[user]
+  name = $1
+  email = $2
+" > $HOME/.gitconfig
+
 # xcode
 xcode-select --install
 
 # dotfiles
-git clone https://github.com/kyoF/dotfiles.git
+git clone git@github.com:kyoF/dotfiles.git
 
 # symbolic link 
 cd $HOME/dotfiles
-./script/ln.sh
+./script/mac/ln.sh
 
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew tap Homebrew/bundle
 brew bundle --file "./script/mac/Brewfile"
 
-# git
-git remote set-url origin git@github.com:kyoF/dotfiles.git
-while true; do
-  read -p "your github user name: " name
-  read -p "your github email address: " mail
-
-  read -p "Correct? [y/n]: " validation
-
-  case $validation in
-    [yY]|[yY][eE][sS])
-      echo "Continue...";
-      break ;;
-    [nN]|[nN][oO])
-      echo "please input again!";;
-    *)
-      echo "faild"
-      echo "please input again!"
-  esac
-done
-echo "[user]
-  name = $name
-  email = $mail
-" > $HOME/.gitconfig
+# line
+mas install 539883307
 
 # macos
 cd $HOME/dotfiles
