fe23abf6fd081ceec15059f10c6912a006ba9151
kyoF <fujiki.kyosuke@gmail.com>
2025-03-19 11:12:39 +0900

    fix: vim keymap

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,9 +1,13 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<C-a>', 'gg<S-v>G', { silent = true })
+vim.keymap.set('n', '<leader><C-a>', 'gg<S-v>G', { silent = true })
 vim.keymap.set('n', 'Y', 'y$', { silent = true })
 
 vim.keymap.set('i', 'jk', '<ESC>', { silent = true })
 vim.keymap.set('i', 'JK', '<ESC>', { silent = true })
 vim.keymap.set('i', 'jK', '<ESC>', { silent = true })
 vim.keymap.set('i', 'Jk', '<ESC>', { silent = true })
+
+for _, quote in ipairs({ '"', "'", "`" }) do
+    vim.keymap.set({ "x", "o" }, "a" .. quote, "2i" .. quote)
+end
